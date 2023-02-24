8cdbad354bf49e06dc801cf34bf29e6e6df9a422
leodan12 <leomacip4@gmail.com>
2023-02-23 23:24:03 -0500

    primer commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+ 
+
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>LEODAN MACHUCA IPARRAGUIRRE</title>
+<link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/responsive.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/animate.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/prettyPhoto.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/font-awesome.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/owl.carousel.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/owl.theme.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/owl.transitions.css') }}" rel="stylesheet" type="text/css" /> 
+ 
+ 
+</head>
+<body data-spy="scroll" data-target=".navbar-default" data-offset="100">
+<!--Preloader-->
+<div id="preloader">
+  <div id="pre-status">
+    <div class="preload-placeholder"></div>
+  </div>
+</div>
+<!--Navigation-->
+<header id="menu">
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+          <a class="navbar-brand" href="#menu"> </a> </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a class="scroll" href="#menu">SOBRE MI</a></li> 
+            <li><a class="scroll" href="#service">CONOCIMIENTOS</a></li> 
+            <li><a class="scroll" href="#portfolio">PROYECTOS RECIENTES</a></li> 
+            <li><a class="scroll" href="#contact">CONTACTO</a></li>
+          </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+      </div>
+      <!-- /.container-fluid -->
+    </div>
+  </div>
+</header>
+<!--Slider-Start-->
+<section id="slider">
+  <div id="home-carousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="item active" style="background-image:url(images/Slider/01.jpg)">
+        <div class="carousel-caption container">
+          <div class="row">
+            <div class="col-md-9 col-sm-12 col-xs-12">
+              <h1>HOLA SOY LEODAN MACHUCA IPARRAGUIRRE</h1>
+              <h2>ING. DE SISTEMAS</h2>
+              <p> EGRESADO DE LA CARRERA DE INGENIERIA DE SISTEMAS DE LA UNIVERSIDAD NACIONAL DE TRUJILLO, PERÚ. 
+                CON CAPACIDAD PARA APRENDER RAPIDAMENTE Y DE APLICAR MIS CONOCIMIENTOS EN EL ÁMBITO PROFESIONAL, 
+                CON VALORES BASADOS EN LA ÉTICA, RESPONSABILIDAD Y COMPROMISO. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+       
+       
+      <a class="home-carousel-left" href="#home-carousel" data-slide="prev"><i class="fa fa-angle-left"></i></a> <a class="home-carousel-right" href="#home-carousel" data-slide="next"><i class="fa fa-angle-right"></i></a> </div>
+  </div>
+  <!--/#home-carousel-->
+</section>
+
+<!--Service-Section-Start-->
+<section id="service">
+  <div class="container">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="heading">
+        <h2>CONOCIMIENTOS  </h2>
+        <div class="line"></div>
+        <p>CUENTO CON SOLIDOS CONOCIMIENTOS EN LAS SIGUIENTES AREAS:</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="features-sec">
+        <div class="col-md-4 col-sm-6 col-xs-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="0ms">
+          <div class="media service-box">
+            <div class="pull-left"> <i class="fa fa-line-chart"></i> </div>
+            <div class="media-body">
+              <h5 class="media-heading">DESARROLLO WEB</h5>
+              <p>USANDO LOS SIGUIENTES FRAMEWORSK Y LENGUAJES: <span> LARAVEL-PHP, DJANGO-PYTHON, SPRING BOOT-JAVA </span></p>
+            </div>
+          </div>
+        </div>
+        <!--/.col-md-4-->
+        <div class="col-md-4 col-sm-6 col-xs-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="100ms">
+          <div class="media service-box">
+            <div class="pull-left"> <i class="fa fa-database" aria-hidden="true"></i> </div>
+            <div class="media-body">
+              <h5 class="media-heading">BASES DE DATOS</h5>
+              <p> USANDO LOS ADMINISTRADORES DE BASES DE DATOS: <span>   SQL SERVER, MYSQL, POSTGRESQL </span> </p>
+            </div>
+          </div>
+        </div>
+        <!--/.col-md-4-->
+        <div class="col-md-4 col-sm-6 col-xs-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="200ms">
+          <div class="media service-box">
+            <div class="pull-left"> <i class="fa fa-desktop" aria-hidden="true"></i> </div>
+            <div class="media-body">
+              <h5 class="media-heading">DESK APPS</h5>
+              <p>USANDO LOS LENGUAJES DE DESARROLLO DE APLICACIONES DE:  <span>   VISUAL BASIC, JAVA </span>  </p>
+            </div>
+          </div>
+        </div>
+        <!--/.col-md-4-->
+        <div class="col-md-4 col-sm-6 col-xs-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="300ms">
+          <div class="media service-box">
+            <div class="pull-left"> <i class="fa fa-bar-chart"></i> </div>
+            <div class="media-body">
+              <h5 class="media-heading">ANALISIS DE DATOS</h5>
+              <p>USANDO LOS PROGRAMAS PARA ANALISIS Y TRATAMIENTO DE DATOS DE: <span> POWER BI, QLICKVIEW </span> </p>
+            </div>
+          </div>
+        </div>
+        <!--/.col-md-4-->
+        <div class="col-md-4 col-sm-6 col-xs-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="400ms">
+          <div class="media service-box">
+            <div class="pull-left"> <i class="fa fa-mobile" aria-hidden="true"></i> </div>
+            <div class="media-body">
+              <h5 class="media-heading">DESARROLLO MOVIL</h5>
+              <p> USANDO LOS SIGUIENTES FRAMEWORKS MULTIPLATAFORMA: <span>FLUTTER, IONIC </span> </p>
+            </div>
+          </div>
+        </div>
+        <!--/.col-md-4-->
+        <div class="col-md-4 col-sm-6 col-xs-6 wow fadeInUp" data-wow-duration="300ms" data-wow-delay="500ms">
+          <div class="media service-box">
+            <div class="pull-left"> <i class="fa fa-check-circle-o" aria-hidden="true"></i> </div>
+            <div class="media-body">
+              <h5 class="media-heading">CODIGO LIMPIO</h5>
+              <p> ESCRITURA DE CODIGO LIMPIO, ORDENADO Y DOCUMENTADO sdgsdfg dsgf dsf srg erg rgse  </p>
+            </div>
+          </div>
+        </div>
+        <!--/.col-md-4-->
+      </div>
+    </div>
+     
+  </div>
+</section>
+ 
+<!--Portfolio-Section-Start-->
+<section id="portfolio">
+  <div class="container">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="heading">
+        <h2>PROYECTOS RECIENTES</h2>
+        <div class="line"></div>
+        <p> A CONTINUACION SE MUESTRAN LOS PROYECTOS QUE HE CREADO RECIENTEMENTE: </p>
+      </div>
+    </div>
+    <div class="text-center">
+      <ul class="portfolio-filter">
+        <li><a class="active" href="#" data-filter="*">All Works</a></li>
+        <li><a href="#" data-filter=".creative">PHP-LARAVEL</a></li>
+      <!--  <li><a href="#" data-filter=".corporate">Corporate</a></li>
+        <li><a href="#" data-filter=".portfolio">Portfolio</a></li>-->
+      </ul>
+      <!--/#portfolio-filter-->
+    </div>
+    <div class="portfolio-items">
+      <div class="col-md-6 col-sm-6 col-xs-12 portfolio-item creative">
+        <div class="portfolio-item-inner"> <img class="img-responsive" src="images/geepperu.jpg" alt="" height="100%">
+          <div class="portfolio-info"> <a  href="https://geepperu2.herokuapp.com/" target="_blank" >
+            <i class="fa fa-eye" aria-hidden="true"> <span style="color: white;"> GEEP PERÚ S.R.L. </span></i>
+            <h6 style="color: white;">CREDENCIALES DE ACCESO: </h6>  </a>
+            <h6 style="color: white;">Usuario: <span style="color: rgb(14, 224, 213);"> admin123_geepperu@gmail.com </span></h6>
+            <h6 style="color: white;">Contraseña:<span style="color: rgb(14, 224, 213);"> password</span></h6>
+         
+          </div>
+        </div>
+      </div>
+      <!--/.portfolio-item-->
+     
+      <!--/.portfolio-item-->
+      <div class="col-md-6 col-sm-6 col-xs-12 portfolio-item creative">
+        <div class="portfolio-item-inner"> <img class="img-responsive" src="images/emcosani.jpg" alt="">
+          <div class="portfolio-info"> <a   href="https://emcosani.herokuapp.com/"  >
+            <i class="fa fa-eye" aria-hidden="true"> <span style="color: white;"> EMCOSANI S.A.C. </span></i>
+            <h6 style="color: white;">CREDENCIALES DE ACCESO: </h6> </a>
+            <h6 style="color: white;">Usuario: <span > mleodan9@gmail.com </span></h6>
+            <h6 style="color: white;">Contraseña:<span> password</span></h6>
+         
+          </div>
+        </div>
+      </div>
+     
+      
+     
+    </div>
+  </div>
+</section>
+ 
+<!--Contact-Section-Start-->
+<section id="contact">
+  <div class="container">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="heading">
+        <h2>CONTACTO</h2>
+        <div class="line"></div>
+        <p>PUEDES UTILIZAR LOS SIGUIENTES MEDIOS PARA CONTACTAR CONMIGO</p>
+      </div>
+    </div>
+    <div class="text-center">
+      <div class="col-md-6 col-sm-6 contact-sec-1">
+        <h4>INFORMACION DE CONTACTO</h4>
+        <ul class="contact-form">
+          <li><i class="fa fa-map-marker"></i>
+            <h6><strong>DIRECCIÓN:</strong> TRUJILLO, PERÚ </h6>
+          </li>
+          <li><i class="fa fa-envelope"></i>
+            <h6><strong>CORREO:</strong> <a href="mailto:leomacip4@gmail.com">leomacip4@gmail.com</a>  </h6>
+          </li>
+          <li><i class="fa fa-phone"></i>
+            <h6><strong>TELEFONO:</strong> <a href="tel:+51 937255241">+51 937255241</a>  </h6>
+          </li>
+          <li><i class="fa fa-linkedin" aria-hidden="true"></i>
+            <h6><strong>LINKEDIN:</strong> <a href="https://www.linkedin.com/in/leodan-machuca-iparraguirre-lmi">leodan-machuca-iparraguirre-lmi</a> </h6>
+          </li>
+          <li>
+            <i class="fa fa-github" aria-hidden="true"></i>
+             <h6><strong>GITHUB:</strong> <a href="https://github.com/leodan12">leodan12</a> </h6>
+          </li>
+        </ul>
+      </div>
+       
+    </div>
+  </div>
+</section>
+ 
+<footer id="footer-down">
+   
+  <p> &copy;  Created By : LEODAN MACHUCA IPARRAGUIRRE </p>
+</footer>
+
+<script src="{{ asset('js/jquery.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/bootstrap.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/jquery.prettyPhoto.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/jquery.nicescroll.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/waypoints.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/jquery.isotope.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/wow.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/jquery.countTo.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/jquery.inview.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/owl.carousel.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/script.js') }}" type="text/javascript"></script>
+ 
+</body>
+</html>
+
+ 
+<script type="text/javascript" src="http://stats.hosting24.com/count.php"></script>
+ 
